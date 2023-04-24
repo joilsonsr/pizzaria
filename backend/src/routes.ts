cb79326313df65cad625959bec2e81798c20d185
joilsonsr <joilsonsr@hotmail.com>
2023-04-23 22:17:34 -0300

    criacaoOrder
    criacação de order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,6 +17,10 @@ import uploadConfig from './config/multer'
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
 import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
+import { SendOrderController } from "./controllers/order/SendOrderController";
+import { ListOrdersController } from "./controllers/order/ListOrdersController";
+import {  DetailOrderController } from "./controllers/order/DetailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"))
@@ -28,7 +32,6 @@ router.get('/me',isAuthenticated, new DetailUserController().handle)
 // --ROTAS CATEGORY 
 router.post('/category',isAuthenticated, new CreateCategoryController().handle)
 router.get('/category',isAuthenticated,new ListCategoryController().handle)
-export{ router};
 
 //-- ROTAS PRODUCT
 router.post('/product', isAuthenticated,upload.single('file'), new CreateProductController().handle)
@@ -39,3 +42,9 @@ router.post('/order',isAuthenticated, new CreateOrderController().handle)
 router.delete('/order',isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add',isAuthenticated,new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders',isAuthenticated,new ListOrdersController().handle)
+router.get('/order/detail',isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish',isAuthenticated,new FinishOrderController().handle)
+
+export{ router};
